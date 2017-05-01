b3a44fbb44d2a9ea60fd055d3034a5f43a68a1f5
Robbie Lopez <rolopez.email@gmail.com>
2017-05-01 16:29:04 -0600

    Merge branch 'master' of C:\Users\LoroloDeuces\Desktop\bootcamp\git\petfoster with conflicts.

---
+++ epic/use-case.php
@@ -0,0 +1,5 @@
+Use Case: Its 1:00 PM on a Sunday and Juan Carlos is enjoying his only day off with his three kids. He already has two
+older dogs (ages 8+) and is looking to add a younger puppy to his family for his kids. Also, he has recently moved Due to lack of time with his schedule
+on his days off he wants to just spend the day with daily errands, friends, and family. He does not have the time nor want
+to check every website in the Albuquerque area or check every pound for a rottweiler puppy. Rottweiler puppies are in huge
+demand and are not up for foster for too long.
