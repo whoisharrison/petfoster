2911d82c94f709cde9bacb7fa7e8535c3268ef0e
Robbie Lopez <rolopez.email@gmail.com>
2017-05-01 16:29:38 -0600

    user case for (a)

--- epic/use-case.php
+++ epic/use-case.php
@@ -1,5 +1,11 @@
 Use Case: Its 1:00 PM on a Sunday and Juan Carlos is enjoying his only day off with his three kids. He already has two
-older dogs (ages 8+) and is looking to add a younger puppy to his family for his kids. Also, he has recently moved Due to lack of time with his schedule
-on his days off he wants to just spend the day with daily errands, friends, and family. He does not have the time nor want
-to check every website in the Albuquerque area or check every pound for a rottweiler puppy. Rottweiler puppies are in huge
-demand and are not up for foster for too long.
+older dogs (ages 8+) and is looking to add a younger puppy to his family for his kids. Also, he has recently moved to a bigger house and wants a bigger guard dog.
+Due to lack of time with his schedule, on his days off he wants to just spend the day with daily errands, friends, and family. He does not have the time nor want
+to check every website or check every pound for a dog in the Albuquerque area.
+
+His perfect dog is going to be a rottweiler puppy. But those dogs are in high demand and do not stay up for foster for too long.
+Juan Carlos has wasted too much time looking on web pages that are not up to date and going to pounds where the dog has already been adopted
+for weeks now. He is fed up with all his time wasted, he just wants to be able to able to enter the information and get some type of
+notification when the puppy he wants is available.
+
+Juan Carlos stumbles upon Rainy Day Adoption...
