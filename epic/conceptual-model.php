48b58cd7da9de8550bc085ab9ef82dda5e54d11d
Michael Harrison <mharrison13@cnm.edu>
2017-05-01 16:28:10 -0600

    3 personas started

---
+++ epic/conceptual-model.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+	<title>Conceptual-Model</title>
+</head>
+<body>
+	<main>
+		<h2>Entities and Attributes</h2>
+		<p><strong>User Profile</strong></p>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<p>Pet Profile</p>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<p><strong>Favorite</strong></p>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<p><strong>Relations</strong></p>
+		<ul>
+			<li>One <strong>Profile </strong>favorites products - (m to n)</li>
+		</ul>
+		<br>
+		<img src="https://bootcamp-coders.cnm.edu/~mharrison13/data-design/public_html/images/erd-data-design.svg">
+	</main>
+</body>
