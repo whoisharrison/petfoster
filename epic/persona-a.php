cc80087f9d29f3692749fe1e2e73774b714a0c3b
Michael Harrison <mharrison13@cnm.edu>
2017-05-01 14:30:40 -0600

    created epic folder and files

---
+++ epic/persona-a.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+
+		<title>Pet Foster ABQ</title>
+		<style></style>
+	</head>
+	<body>
+		<h1>Pet Foster ABQ</h1>
+		<h2>Purpose, Audience, Goal</h2>
+
+		<strong>Purpose:</strong> Professional and well developed webpage that easily navigates a user to either adopt or put their pet up for adoption
+		<strong>Audience:</strong> General population
+		<strong>Goal:</strong> To be the #1 web page that users go to got their pet adoption needs.
+
+		<h2>PERSONA</h2>
+		<strong>Name:</strong> Juan Carlos Garcia
+		<strong>Age:</strong> 35
+		<strong>Profession:</strong>
+		<p>Hotel manager within the Albuquerque area, specializes in catering and customer service</p>
+
+		<p>Technology: Basic Microsoft OS knowledge, Basic Samsung Galaxy 7 experience, just has wifi at home for his phone and basic television watching apps. Has a dell computer and laptop in his office at work. Previous dog owner and trainer.</p>
+
+		<p>Attitudes and Behaviors: Single parent of three children, puts in 50+ hours in of work a week, always busy and always on the go, never a set schedule. Wants things done as easily and as fast as possible. Prefers shopping online over in stores. Wants to spend the least amount of time possible on a website for his needs</p>
+
+		<strong>Frustrations and Needs:</strong>
+		<p>Adopting a pet: databases not up to date, can not trust where the dog came from, good
+			search engine, notification if pet is available, no time to go to every shelter and/or every
+			shelters web page</p>
+		<p>Putting a pet up for adoption: can not trust the people adopting, notification someone
+			wants their pet</p>
+
+		<Strong>Goals:</Strong>
+		<p>Adopting a pet: To have a database with up to date and accurate information on pets in the surrounding area that are up for adoption, that will quickly notify users of the pets availability.</p>
+		<p>Putting a pet up for audition: To give a user an easy and quick experience in adding their pet to our database and notifying them when a trusted foster parent has interest</p>
+	</body>
