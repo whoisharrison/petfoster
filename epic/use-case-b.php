d62d800b624272aaace4f53825e442826e5bb901
Michael Harrison <mharrison13@cnm.edu>
2017-05-02 07:58:04 -0600

    Use Case for persona b (pet seeker)

--- epic/use-case-b.php
+++ epic/use-case-b.php
@@ -9,4 +9,5 @@
 	</head>
 	<body>
 		<h1>Pet Foster ABQ</h1>
+		<p>It is 6:05 p.m. and Chloe is using her families desk top to search for a site that will help her locate placement for the dog. As she only has a short time before she must stop using the internet and do her homework, she dismissed sites that seem to complicated. She is looking for a site that displays relevant information and instructions that are clearly displayed and easy to access.</p>
 	</body>
