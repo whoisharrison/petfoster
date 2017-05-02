1f588776da30fc794dc786a72b7e53bfec07944a
Robbie Lopez <rolopez.email@gmail.com>
2017-05-01 19:14:31 -0600

    flow for use-case

--- epic/use-case.php
+++ epic/use-case.php
@@ -9,3 +9,29 @@ for weeks now. He is fed up with all his time wasted, he just wants to be able t
 notification when the puppy he wants is available.
 
 Juan Carlos stumbles upon Rainy Day Adoption...
+
+
+
+FLOW
+
+Juan Carlos googles #1 pet adoption service
+User clicks top link on page Rainy Day Pet Adoption
+Webpage opens up and user clicks ADOPT A PET
+User navigated to a form page to create a profile as a new user
+User enters all necessary and/or optional information to create a profile and clicks enter (email and/or phone #)
+User is sent a token to confirm creation of new profile
+User enters activation code and is sent to the ADOPT A PET page
+User enters information in form page such as type of animal, type of breed, age, sex, etc...
+Information is searched through the database and user is sent to a page with results that best fits users inputs
+
+User then clicks an animal image that they want or interested in
+	Page opens up with more information about the animal, including contact information on the current owner
+	User will decide they want the animal and contact the current owner or go back to previous search results
+
+or
+
+Clicks the "contact me when animal is available" option
+	User is then sent to another page that says "Thanks we will contact you as soon as a better option becomes available"
+
+
+
