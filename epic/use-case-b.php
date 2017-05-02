84fcdd93a16a6388542b936e95a62f40e8623b72
Michael Harrison <mharrison13@cnm.edu>
2017-05-02 12:14:06 -0600

    Use Case for persona b (pet seeker)

--- epic/use-case-b.php
+++ epic/use-case-b.php
@@ -9,5 +9,19 @@
 	</head>
 	<body>
 		<h1>Pet Foster ABQ</h1>
-		<p>It is 6:05 p.m. and Chloe is using her families desk top to search for a site that will help her locate placement for the dog. As she only has a short time before she must stop using the internet and do her homework, she dismissed sites that seem to complicated. She is looking for a site that displays relevant information and instructions that are clearly displayed and easy to access.</p>
+		<p>It is 6:05 p.m. and Chloe, with the help of her father, is using her families desk top to search for a site that will help her locate placement for the dog. As she only has a short time before she must stop using the internet and do her homework, she dismissed sites that seem to complicated. She is looking for a site that displays relevant information and instructions that are clearly displayed and easy to access, she comes across Pet Adopt ABQ</p>
+
+		<ol>
+			<li>User goes to Foster website.</li>
+			<li>Page Loads landing page.</li>
+			<li>User selects <em>post pet for adoption</em></li>
+			<li>Page prompts user to sign in to post a pet.</li>
+			<li>User creates an account and verifies through email</li>
+			<li>Page delivers description fields and pet info for user to enter</li>
+			<li>User enters information in form page such as animal size and breed.</li>
+			<li>Page prompts user to enter a picture of pet</li>
+			<li>User uploads pet image and submits</li>
+			<li>Page delivers confirmation and lets user know they will be contacted via email when an adopter is found.</li>
+		</ol</body>
+
 	</body>
