81729bf2913ea0b6f4423209ae0bda1c5c1c4720
Jabari Farrar <jofarrar@gmail.com>
2017-05-10 19:35:20 -0600

    initialized PostTest https://app.asana.com/0/334403035277160/335518406456412

---
+++ php/classes/test/PostTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Edu\Cnm\PetRescueAbq\Test;
+
+use Edu\Cnm\Petfoster\Test\PetRescueAbqTest;
+use Edu\Cnm\PetRescueAbq\{Profile, Organization, Message, Post};
+
+//grab the class under scrutiny
+require_once (dirname(__DIR__) . "/autoload.php");
+
+/** Full PHPUnit test for the Post Class
+ *
+ *This is a complete PHPUnit test of the Post class. It is complete because *ALL* my SQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Post
+ * @author Jabari Farrar <tmafm1@gmail.com>
+ *
+ **/
+class PostTest extends PetRescueAbqTest {
+
+
+}
+
