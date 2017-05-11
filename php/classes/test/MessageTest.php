cd784c45597d00e2389066a1270d2851e66659be
Robbie Lopez <rolopez.email@gmail.com>
2017-05-11 00:30:08 -0600

    getting unit  testing done

--- php/classes/test/MessageTest.php
+++ php/classes/test/MessageTest.php
@@ -70,15 +70,15 @@ class MessageTest extends PetRescueAbqTest {
 
 
 	/**
- * create dependent objects before running each test
- */
-public final function setUp() : void {
+ 	* create dependent objects before running each test
+ 	*/
+	public final function setUp() : void {
 
-	// run the default setUp() method first
-	parent::setUp();
-	$password = "asdfasdf";
-	$this->VALID_SALT = bin2hex(random_bytes(32));
-	$this->VALID__HASH = hash_pbkdf2("sha512", $password, $this->VALID_SALT, 262144);
+		// run the default setUp() method first
+		parent::setUp();
+		$password = "asdfasdf";
+		$this->VALID_SALT = bin2hex(random_bytes(32));
+		$this->VALID__HASH = hash_pbkdf2("sha512", $password, $this->VALID_SALT, 262144);
 
 	// create and insert a Organization to own the test Message
 	$this->organization = new Organization(null, null, "@handle", "test@phpunit.de", 	$this->VALID_HASH, "+15055553333", $this->VALID_SALT);
@@ -98,19 +98,94 @@ public final function setUp() : void {
 	//format the sunset date to use for testing
 	$this->VALID_SUNSETDATE = new\DateTime();
 	$this->VALID_SUNSETDATE->add(new \DateInterval("P10D"));
-}
+	}
 
 
 /**
- * test insetring a valid Message and verify that the actual mySQL data matches
+ * test inserting a valid Message and verify that the actual mySQL data matches
  */
 public function testInsertValidMessage() : void {
 	//count the number of rows and save it for later
 	$numRows = $this->getConnection()->getRowCount("message");
 
 	//create a new Message and insert into mySQL
-	$message = new Message(null, $this->)
+	$message = new Message(null, $this->organization->getOrganizationId(), $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEDATE);
+	$message->insert($this->getPDO());
+
+	//grab the data from mySQL and enforce the fields match our expectations
+	$pdoMessage = Message::getMessageByMessageId($this->getPDO(), $message->getMessageId());
+	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("message"));
+
+	$this->assertEquals($pdoMessage->getMessageOrganizationId(), $this->organization->getOrganizationId());
+	$this->assertEquals($pdoMessage->getMessageProfileId(), $this->profile->getProfileId());
+
+	$this->assertEquals($pdoMessage->getMessageContent(), $this->VALID_MESSAGECONTENT);
+
+	//date to seconds since the beginning of time to avoid round off error
+	$this->assertEquals($pdoMessage->getMessageDate()->getTimestamp(), $this->VALID_MESSAGEDATE->getTimestamp());
+
+	$this->assertEquals($pdoMessage->getMessageSubject(), $this->VALID_MESSAGESUBJECT);
 }
+
+
+/**
+ * test inserting a Message that already exists
+ * @expectedException \PDOException
+ */
+public function testInsertInvalidMessage() : void {
+	//create a Message with a non null message id and watch it fail
+
+	$message = new Message(DataDesignTest::INVALID_KEY, $this->organization->getOrganizationId(), $this->profile->getProfileId(), $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEDATE, $this->VALID_MESSAGESUBJECT);
+	$message->insert($this->getPDO());
+}
+
+
+/**
+ * test inserting a Message, editing it, and then updating it
+ */
+public function testUpdateValidMessage() : void {
+	//count the number of rows and save it for later
+	$numRows = $this->getConnection()->getRowCount("message");
+
+	//create a new Message and insert it into mySQL
+	$message = new Message(null, $this->organization->getOrganizationId(), $this->profile->getProfileId(), $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEDATE, $this->VALID_MESSAGESUBJECT);)
+	$message->insert($this->getPDO());
+
+	//edit the Message and update it in mySQL
+	$message->setMessageContent($this->VALID_MESSAGECONTENT);
+
+	//DO I NEED THIS???
+	$message->setMessageSubject($this->VALID_MESSAGESUBJECT);
+
+	$message->update($this->getPDO());
+
+	//grab the data from mySQL and enforce the fields match our expectations
+	$pdoMessage = Message::getMessageByMessageId($this->getPDO(), $message->getMessageId());
+	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("message"));
+
+	$this->assertEquals($pdoMessage->getMessageOrganizationId(), $this->organization->getOrganizationId());
+	$this->assertEquals($pdoMessage->getMessageProfileId(), $this->profile->getProfileId());
+
+	$this->assertEquals($pdoMessage->getMessageContent(), $this->VALID_MESSAGECONTENT);
+
+	//date to seconds since the beginning of time to avoid round off error
+	$this->assertEquals($pdoMessage->getMessageDate()->getTimestamp(), $this->VALID_MESSAGEDATE->getTimestamp());
+
+	$this->assertEquals($pdoMessage->getMessageSubject(), $this->VALID_MESSAGESUBJECT);
+}
+
+
+/**
+ * test updating a Message that already exists
+ * @expectedException \PDOException
+ */
+public function testUpdateInvalidMessage() : void {
+
+	//create a Message with a non null message id and watch it fail
+	$message = new Message(null, $this->organization->getOrganizationId(), $this->profile->getProfileId(), $this->VALID_MESSAGECONTENT, $this->VALID_MESSAGEDATE, $this->VALID_MESSAGESUBJECT);)
+	$message->insert($this->getPDO());
+}
+
 
 
 
