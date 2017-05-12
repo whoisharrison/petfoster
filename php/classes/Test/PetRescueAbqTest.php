0f7f6afdb853085977fd7037d3f2f25f77393e62
Michael Harrison <mharrison13@cnm.edu>
2017-05-12 09:45:21 -0600

    fixed naming in PetRescueAbqTest
    https://app.asana.com/0/334403035277160/335518406456410

--- php/classes/Test/PetRescueAbqTest.php
+++ php/classes/Test/PetRescueAbqTest.php
@@ -92,8 +92,8 @@ abstract class PetRescueAbqTest extends TestCase {
 		// if the connection hasn't been established, create it
 		if($this->connection === null) {
 			// connect to mySQL and provide the interface to PHPUnit
-			$config = readConfig("/etc/apache2/capstone-mysql/petfosterABQ.ini");
-			$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/petfosterABQ.ini");
+			$config = readConfig("/etc/apache2/capstone-mysql/petRescueABQ.ini");
+			$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/petRescueABQ.ini");
 			$this->connection = $this->createDefaultDBConnection($pdo, $config["database"]);
 		}
 		return($this->connection);
