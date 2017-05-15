5d66703bfa63b19350e57b2f077ccc56faf21565
Michael Harrison <mharrison13@cnm.edu>
2017-05-15 15:42:31 -0600

    angular rampup & files
    fixed and changed angular example to petrescueabq
    
    https://app.asana.com/0/334403035277160/335518406456410

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from "@angular/core";
 
 @Component({
-	selector: "angular4-example",
-	templateUrl: "./templates/angular4-example-app.php"
+	selector: "petrescueabq",
+	templateUrl: "./templates/petrescueabq-app.php"
 })
 
 export class AppComponent {}
