c17ac64314db389341b05a863d56bd815ab2d568
Jeffrey Cooper <jcooper37@cnm.edu>
2017-05-17 14:13:30 -0600

    repair org test line 295 email to id

--- php/classes/Test/OrganizationTest.php
+++ php/classes/Test/OrganizationTest.php
@@ -292,7 +292,7 @@ class OrganizationTest extends PetRescueAbqTest {
 		$organization->insert($this->getPDO());
 
 		// grab the data from mySQL and enforce the fields match our expectations
-		$pdoOrganization = Organization::getOrganizationByOrganizationId($this->getPDO(), $organization->getOrganizationEmail());
+		$pdoOrganization = Organization::getOrganizationByOrganizationId($this->getPDO(), $organization->getOrganizationId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("organization"));
 		$this->assertEquals($pdoOrganization->getOrganizationProfileId(), $this->profile->getProfileId());
 		$this->assertEquals($pdoOrganization->getOrganizationActivationToken(), $this->VALID_ACTIVATION);
