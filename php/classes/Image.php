5dbc2bf92cf4aeb8eb475e3648812583f0b142f7
amy skidmore <askidmore1@cnm.edu>
2017-05-22 16:52:39 -0600

    same error w/ row/postid

--- php/classes/Image.php
+++ php/classes/Image.php
@@ -255,7 +255,7 @@ class Image implements \JsonSerializable {
 				$statement->setFetchMode(\PDO::FETCH_ASSOC);
 				$row = $statement->fetch();
 				if($row !== false) {
-					$image = new Image($row["imageId"], $row["imagePost"], $row["imageCloudinaryId"]);
+					$image = new Image($row["imageId"], $row["imagePostId"], $row["imageCloudinaryId"]);
 				}
 			} catch(\Exception $exception) {
 				//if the row couldn't be converted, rethrow it
