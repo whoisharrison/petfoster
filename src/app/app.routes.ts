24cf22257ba5d2979520aa50693fa0d7473154b6
Robbie Lopez <rolopez.email@gmail.com>
2017-06-08 23:37:41 -0600

    added messages to rout https://app.asana.com/0/334403035277160/335518406456411

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import {RouterModule, Routes} from "@angular/router";
 import {BaconComponent} from "./components/bacon-component";
 import {HomeComponent} from "./components/home-component";
-import {BaconService} from "./services/bacon-service";
+import {BaconService} from "./services/ba{
 
-export const allAppComponents = [BaconComponent, HomeComponent];
+export const allAppComponents = [BaconComponent, HomeComponent, MessagesComponent];
 
 export const routes: Routes = [
 	{path: "bacon", component: BaconComponent},
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "messages",component: MessagesComponent},
+
 ];
 
 export const appRoutingProviders: any[] = [BaconService];
+
+// not sure if we needed this?
+// export const appRoutingProviders: any[] = [MessageService];
 
 export const routing = RouterModule.forRoot(routes);
---
+++ src/app/components/messages.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+import {MessageService} from "../services/message.service";
+import {Message} from "../classes/message";
+import {Status} from "../classes/status";
+
+@Component({
+	templateUrl: "./templates/messages.php"
+})
+
+export class MessagesComponent implements OnInit {
+
+	newMessage : Message = new Message(null, null, null, null);
+	messages : Message[] = [];
+	status : Status = null;
+
+	constructor(private messageService : MessageService) {}
+
+	ngOnInit() : void {
+		this.getAllMessages();
+	}
+
+	createMessage() : void {
+		this.messageService.createMessage(this.newMessage)
+			.subscribe(status => this.status = status);
+	}
+
+	getAllMessages() : void {
+		this.messageService.getAllMessages()
+			.subscribe(messages => this.messages = messages);
+	}
+}
