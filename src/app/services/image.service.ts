494579c16e180db8ec21bb72dc8b69af4811a505
amy skidmore <askidmore1@cnm.edu>
2017-06-10 20:49:49 -0600

    image service ts

---
+++ src/app/services/image.service.ts
@@ -0,0 +1,39 @@
+
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base.service";
+import {Image} from "../classes/Image";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class ImageService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private imageUrl = "api/image/";
+
+
+	createImage(image: Image): Observable<Status> {
+		return (this.http.post(this.imageUrl, image)
+			.map(this.extractTomWu)
+			.catch(this.handleError));
+	}
+
+	getImageByImageid(imageId: number): Observable<Image[]> {
+		return (this.http.get(this.imageUrl + imageId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getImageByCloudinary(imageCloudinaryId: string): Observable<Image> {
+		return (this.http.get(this.imageUrl + "?imageCloudinaryId=" + imageCloudinaryId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+
+
+
+}
