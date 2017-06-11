00426a4e6135d5fb4d17d552fe3fc3663443abf9
Jeffrey Cooper <jcooper37@cnm.edu>
2017-06-11 15:25:28 -0600

    Static-ui organization coded.

--- static-ui/organization.php
+++ static-ui/organization.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: aloha
- * Date: 09-Jun-17
- * Time: 12:24 AM
- */
+<?php require_once("lib/head-utils.php");?>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- insert header and navbar -->
+		<?php require_once("lib/header.php");?>
+		<main class="bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<h1>Organizations</h1>
+
+					</div>
+
+					<div class="col-md-8">
+						<h1>PetRescueABQ Organization</h1>
+
+						<!-- Begin Post Item -->
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<div class="h4">Organization Name <small>// organization.license</small></div>
+							</div>
+							<div class="panel-body">
+								This is the organization content.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div><!--/.sfooter-content-->
+
+	<!-- insert footer -->
+	<?php require_once("lib/footer.php");?>
+</body>
+</html>
