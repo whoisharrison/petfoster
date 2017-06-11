d44a42d5fb3104bd7e91bd19a9d90df2fcdad645
Robbie Lopez <rolopez.email@gmail.com>
2017-06-11 15:31:41 -0600

    fixed static sign in page https://app.asana.com/0/334403035277160/335518406456411

--- static-ui/signin.php
+++ static-ui/signin.php
@@ -12,43 +12,40 @@
 			<span class="close">&times;</span>
 			<h2>Pet Resecue ABQ Header</h2>
 		</div>
+		<div class="modal-body">
 
-		<form #signInForm="ngForm" name="signInForm" id="signInForm"
-				(ngSubmit)="signIn();">
 
-			<div class="modal-body">
-				<div class="container">
-					<h2 align="center">Authentication</h2>
-					<div class="center">
-						<form method="post" action="/signin" class="form-horizontal" role="form" align="center">
-							<div class="form-group" align="center">
-								<label class="control-label col-sm-2" for="email">Email:<em>*</em></label>
-								<div class="col-sm-6">
-
-									<!--										how do I add org into here??-->
-									<input type="text" name="email" id="email" placeholder="email" required [(ngModel)]="signIn.profileEmail" #profileEmail="ngModel" class="form-control" />
-
-								</div>
+			<div class="container">
+				<h2 align="center">Authentication</h2>
+				<div class="center">
+					<form method="post" action="/signin" class="form-horizontal" role="form" align="center">
+						<div class="form-group" align="center">
+							<label class="control-label col-sm-2" for="username">Username:<em>*</em></label>
+							<div class="col-sm-6">
+								<input type="text" name="username" id="username" placeholder="username" required="true" class="form-control" />
 							</div>
-							<div class="form-group">
-								<label class="control-label col-sm-2" for="password">Password:<em>*</em></label>
-								<div class="col-sm-6">
-
-									<input type="password" name="password" id="password" required [(ngModel)]="signIn.profilePassword" #profilePassword="ngModel" class="form-control" />
-
-								</div>
+						</div>
+						<div class="form-group">
+							<label class="control-label col-sm-2" for="password">Password:<em>*</em></label>
+							<div class="col-sm-6">
+								<input type="password" name="password" id="password" required="true" class="form-control" />
 							</div>
-							<div class="form-group">
-								<div class="col-sm-offset-2 col-sm-8">
-									<input type="submit" name="signin" id="signin" [disabled]="signInForm.invalid" value="Sign In" class="btn btn-default" />
-								</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-8">
+								<input type="submit" name="signin" id="signin" value="Sign In" class="btn btn-default" />
 							</div>
-						</form>
-					</div>
+						</div>
+					</form>
 				</div>
 			</div>
-				<div class="modal-footer">
-					<h3>Footer</h3>
-				</div>
+
+
+
+		</div>
+		<div class="modal-footer">
+			<h3>Footer</h3>
+		</div>
 	</div>
+
 </div>
