8fc6dcb41da4aeb284c0133204d2bdd3d2a6dc79
Jeffrey Cooper <jcooper37@cnm.edu>
2017-06-11 15:47:15 -0600

    Organization angular (template, component, service) pushed.

---
+++ public_html/templates/organization.php
@@ -0,0 +1,24 @@
+<main class="bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h1>Organizations</h1>
+
+			</div>
+
+			<div class="col-md-8">
+				<h1>PetRescueABQ Organization</h1>
+
+				<!-- Begin Post Item -->
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<div class="h4">Organization Name <small>// organization.license</small></div>
+					</div>
+					<div class="panel-body">
+						This is the organization content.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,16 @@ import {BaconService} from "./services/bacon-service"
 import {SignInComponent} from "./components/sign-in.component";
 import {MessagesComponent} from "./components/messages.component";
 import {PostComponent} from "./components/post.component";
+import {OrganizationComponent} from "./components/organization.component";
 
-export const allAppComponents = [BaconComponent, HomeComponent, MessagesComponent, SignInComponent];
+export const allAppComponents = [BaconComponent, HomeComponent, MessagesComponent, SignInComponent, OrganizationComponent];
 
 export const routes: Routes = [
 	{path: "bacon", component: BaconComponent},
 	{path: "", component: HomeComponent},
 	{path: "messages",component: MessagesComponent},
-	{path: "signin", component: SignInComponent}
+	{path: "signin", component: SignInComponent},
+	{path: "organization", component: OrganizationComponent}
 
 ];
 
