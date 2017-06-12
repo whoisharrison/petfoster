4ba969f4b91fcfb0fe812a3ba3cc8f9a44fda85b
amy skidmore <askidmore1@cnm.edu>
2017-06-11 21:16:43 -0600

    footer copyright

--- static-ui/lib/footer.php
+++ static-ui/lib/footer.php
@@ -1,11 +1,8 @@
 <footer class="navbar-fixed-bottom">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6">
-				Footer text.
-			</div>
-			<div class="col-sm-6 text-right-sm">
-				PetRescueABQ
+			<div class="col-xs-12">
+				<div class="fa fa-copyright" style= color:mediumblue aria-hidden="true"> PetRescue2017</div>
 			</div>
 		</div>
 	</div>
