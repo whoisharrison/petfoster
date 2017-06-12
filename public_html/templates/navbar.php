25ef8c26efdf405b4617128bf72a364a65bf79e1
Jeffrey Cooper <jcooper37@cnm.edu>
2017-06-12 10:23:42 -0600

    repair lib/header, public_html/templates/navbar

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -11,20 +11,20 @@
 					<span class="icon-bar"></span>
 				</button>
 				<a class="navbar-brand" href="../images/petrescue3-icon-20px-xs.gif" style="color: mediumblue font-family: 'Handlee' href="">PetRescueABQ</a>
-			</div>
+		</div>
 
 
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-				<ul class="nav navbar-nav navbar-right"style="color: mediumblue>
+			<ul class="nav navbar-nav navbar-right"style="color: mediumblue>
 					<li><a href=""><i class="fa fa-home"></i></a></li>
-				<li><a href="../signup.php" style="color: mediumblue">Sign-Up</a></li>
-				<li><a href="../signin.php" style="color: mediumblue">Sign-In</a></li>
-				<li><a href="../signout.php" style="color: mediumblue">Sign-Out</a></li>
+			<li><a routerLink="../signup.php" style="color: mediumblue">Sign-Up</a></li>
+			<li><a routerLink="../signin.php" style="color: mediumblue">Sign-In</a></li>
+			<li><a routerLink="../signout.php" style="color: mediumblue">Sign-Out</a></li>
 
-				</ul>
+			</ul>
 
 			</div><!-- /.navbar-collapse -->
 		</div>	<!-- /.container-fluid -->
