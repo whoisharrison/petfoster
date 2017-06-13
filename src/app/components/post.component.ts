d5b75b86e1f28b5b3f81cc298ae024b19749ea05
Robbie Lopez <rolopez.email@gmail.com>
2017-06-12 19:37:28 -0600

    moved image to the trash
    https://app.asana.com/0/334403035277160/335518406456411

--- src/app/components/post.component.ts
+++ src/app/components/post.component.ts
@@ -3,21 +3,39 @@ import {ActivatedRoute, Params} from "@angular/router";
 import {PostService} from "../services/post.service";
 import {Post} from "../classes/post";
 import {Status} from "../classes/status";
+import {FileUploader} from "ng2-file-upload";
+import {Cookie} from "ng2-cookies";
+import {Observable} from "rxjs";
+import "rxjs/add/observable/from";
 
 @Component({
 	selector: 'post',
-	templateUrl: "./templates/post-template.php"
+	templateUrl: "./templates/post.php"
 })
 
 export class PostComponent implements OnInit{
+	public uploader: FileUploader = new FileUploader({
+		itemAlias: "dog",
+		url: "./api/image/",
+		headers: [{name: "X-XSRF-TOKEN", value: Cookie.get("XSRF-TOKEN")}],
+		additionalParameter: {}
+	});
 	posts: Post[] = [];
 	status: Status = null;
 	post: Post = new Post(null, null, null, null, null, null, null);
+	protected cloudinaryPublicId : string = null;
+	protected cloudinaryPublicIdObservable : Observable<string> = new Observable<string>();
+
 	constructor(private postService: PostService, private route: ActivatedRoute) {
 	}
 
 
 	ngOnInit(): void {
+		this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any) => {
+			let reply = JSON.parse(response);
+			this.cloudinaryPublicId = reply.data;
+			this.cloudinaryPublicIdObservable = Observable.from(this.cloudinaryPublicId);
+		};
 		this.getAlmostAllPosts();
 	}
 
