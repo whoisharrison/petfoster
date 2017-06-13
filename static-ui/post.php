2081f48c896e1a376733b666f42f3db612bbed71
Jeffrey Cooper <jcooper37@cnm.edu>
2017-06-12 19:59:14 -0600

    Update post.php in static and template

--- static-ui/post.php
+++ static-ui/post.php
@@ -1,58 +1,33 @@
 <?php require_once("lib/head-utils.php"); ?>
 <main>
 	<form class="form-horizontal" name="imageUpload" id="post" (submit)="uploadImage();">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<h1>Post</h1>
-					<h4>Choose pet type</h4>
-					<fieldset id="type">
-						<label class="radio-inline"><input type="radio" name="type"> Dog</label>
-						<label class="radio-inline"><input type="radio" name="type"> Cat</label>
-					</fieldset>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<h4>Choose the gender</h4>
-					<fieldset id=sex">
-						<label class="radio-inline"><input type="radio" name="gender"> Female</label>
-						<label class="radio-inline"><input type="radio" name="gender"> Male</label>
-						<fieldset>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-						<h4> Please enter the breed</h4>
-						<label for="formGroupEnterBreed"></label>
-						<input type="text" class="form-control" id="formGroupExampleInput" placeholder="Breed Type">
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-						<h4>Please enter pet description</h4>
-						<label for="exampleTextarea"></label>
-						<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
- 						<label for="postImage" class="modal-labels">Upload an image</label>
-						<input type="file" name="dog" id="dog" ng2FileSelect [uploader]="uploader"/>
-					</div>
-				<div class="row">
-							<div class="col-md-6">
-								<div class="form-group">
-									<button type="submit" class="btn-link fa fa-paw fa-2x"><i class="fa fa-file-image-o" aria-hidden="true"></i>Submit
-									</button>
-								</div>
-							</div>
-				</div>
-			</div>
+		<h1>Post</h1>
+		<h4>Choose pet type</h4>
+		<fieldset id="type">
+			<label class="radio-inline"><input type="radio" name="type"> Dog</label>
+			<label class="radio-inline"><input type="radio" name="type"> Cat</label>
+		</fieldset>
+		<h4>Choose the gender</h4>
+		<fieldset id=sex">
+			<label class="radio-inline"><input type="radio" name="gender"> Female</label>
+			<label class="radio-inline"><input type="radio" name="gender"> Male</label>
+		</fieldset>
+		<div class="form-group">
+			<h4> Please enter the breed</h4>
+			<label for="formGroupEnterBreed"></label>
+			<input type="text" class="form-control" id="formGroupExampleInput" placeholder="Breed Type">
+		</div>
+		<div class="form-group">
+			<h4>Please enter pet description</h4>
+			<label for="exampleTextarea"></label>
+			<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+		</div>
+		<div>
+			<label for="postImage" class="modal-labels">Upload an image</label>
+			<input type="file" name="dog" id="dog" ng2FileSelect [uploader]="uploader"/>
+		</div>
+		<div class="form-group">
+			<button type="submit" class="btn-link fa fa-paw fa-2x"><i class="fa fa-file-image-o" aria-hidden="true"></i>Submit</button>
 		</div>
 	</form>
 	<p>Cloudinary Public Id: {{ cloudinaryPublicId }}</p>
