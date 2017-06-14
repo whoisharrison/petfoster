70b3735e8854bc09a42e973ab05d3b874e1473f8
Rochelle Lewis <rlewis37@gmail.com>
2017-06-14 11:20:08 -0600

    @rlewis37 added results template, css

--- public_html/templates/result.php
+++ public_html/templates/result.php
@@ -1,12 +1,33 @@
 <main class="bg">
 	<div class="container">
 		<div class="row">
-
-			<div class="col-md-4">
-				<h1 id="profileinfo">Create New Post</h1>
-
-
+			<div class="col-xs-12">
+				<h2>Results</h2>
 			</div>
 		</div>
-	</div>
+
+		<!-- BEGIN POST ITEM -->
+		<article class="search-result row">
+
+			<div class="col-xs-12 col-sm-12 col-md-3">
+				<a href="#" title="Lorem ipsum" class="thumbnail"><img src="http://lorempixel.com/250/140/people" alt="Lorem ipsum" /></a>
+			</div>
+
+			<div class="col-xs-12 col-sm-12 col-md-2">
+				<ul class="meta-search">
+					<li><i class="glyphicon glyphicon-calendar"></i> <span>02/15/2014</span></li>
+					<li><i class="glyphicon glyphicon-time"></i> <span>4:28 pm</span></li>
+					<li><i class="glyphicon glyphicon-tags"></i> <span>People</span></li>
+				</ul>
+			</div>
+
+			<div class="col-xs-12 col-sm-12 col-md-7 excerpet">
+				<h3><a href="#" title="">Voluptatem, exercitationem, suscipit, distinctio</a></h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, exercitationem, suscipit, distinctio, qui sapiente aspernatur molestiae non corporis magni sit sequi iusto debitis delectus doloremque.</p>
+				<span class="plus"><a href="#" title="Lorem ipsum"><i class="glyphicon glyphicon-plus"></i></a></span>
+			</div>
+			<span class="clearfix borda"></span>
+		</article>
+
+	</div><!--/.container-->
 </main>
